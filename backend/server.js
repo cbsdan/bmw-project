2291030b3d91ca6b7525a565750a74d0ebdab779
Daniel Cabasa <cabasadaniel1@gmail.com>
2024-11-24 03:55:19 +0800

    add frontend cors

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,12 @@ app.use(express.json({limit:'50mb'}))
 app.use(express.urlencoded({limit: "50mb", extended: true }));
 app.use(logger)
 
-app.use(cors())
+app.use(cors({
+    origin: ["https://borrow-my-wheel.vercel.app/", "http://localhost:5173"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true
+}))
+
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use(errorHandler)
 
